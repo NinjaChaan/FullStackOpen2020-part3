36b86638e8a0038b01be30a9b3d1a97da68043d0
Ninya <sleepyneko@protonmail.ch>
2020-04-14 12:44:14 +0300

    changed port

--- Phonebook-backend/index.js
+++ Phonebook-backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 morgan.token('data', (request) => JSON.stringify(request.body))
@@ -97,7 +98,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
