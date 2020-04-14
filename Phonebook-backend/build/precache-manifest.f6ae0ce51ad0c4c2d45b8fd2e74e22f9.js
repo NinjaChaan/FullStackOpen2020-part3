07692302353fdbdf0ef27aa2413346e21e95b813
Ninya <sleepyneko@protonmail.ch>
2020-04-14 13:45:35 +0300

    Build

---
+++ Phonebook-backend/build/precache-manifest.f6ae0ce51ad0c4c2d45b8fd2e74e22f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e491b2f21c2eebfb4b985e61689d694d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8f76cb1fc8cb9f4a0e2",
+    "url": "/static/css/main.3496398d.chunk.css"
+  },
+  {
+    "revision": "c8806c721a9ce1bb3bb5",
+    "url": "/static/js/2.1c4526c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c4526c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f76cb1fc8cb9f4a0e2",
+    "url": "/static/js/main.5dc4b430.chunk.js"
+  },
+  {
+    "revision": "d19032c1263fa01ea41a",
+    "url": "/static/js/runtime-main.08633c47.js"
+  }
+]);
