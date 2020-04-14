0c758772ff23a6110dcfca93266017f45719611b
Ninya <sleepyneko@protonmail.ch>
2020-04-14 11:52:39 +0300

    procfile

--- Phonebook-backend/index.js
+++ Phonebook-backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 morgan.token('data', (request) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
